e868c86dc0e7379ee7baeae432cff41fe49714f1
Frankenstine Joe <Frankenstine@DESKTOP-KCNC5C4>
2018-05-13 21:30:52 +0500

    Add project files.

---
+++ Dependency Injection using Unity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dependency_Injection_using_Unity.Startup))]
+namespace Dependency_Injection_using_Unity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
